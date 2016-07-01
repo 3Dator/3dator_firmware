1a14f627aa10739609fe80b87f374c80d895ecd3
Moritz Mair <mail@moritzmair.de>
2016-07-01 16:31:54 +0200

    changed pla preheat settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -565,9 +565,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = true;  // set to true to invert the logic o
 //#define EEPROM_CHITCHAT
 
 // Preheat Constants
-#define PLA_PREHEAT_HOTEND_TEMP 180
-#define PLA_PREHEAT_HPB_TEMP 70
-#define PLA_PREHEAT_FAN_SPEED 255   // Insert Value between 0 and 255
+#define PLA_PREHEAT_HOTEND_TEMP 200
+#define PLA_PREHEAT_HPB_TEMP 40
+#define PLA_PREHEAT_FAN_SPEED 0   // Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 240
 #define ABS_PREHEAT_HPB_TEMP 100
